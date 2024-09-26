6f0de82eba793cf5279c094e3db4ec8e74e7c9c6
Khirushan-Sasitharan-ICD2O1-01 <khirushan.sasitharan@student.tdsb.on.ca>
2024-09-26 14:04:30 +0000

    hbhbhvb

--- PP1_7.py
+++ PP1_7.py
@@ -1,25 +1,40 @@
 
 
-q1():
-  #Write code here
+def q1():
+  bool = True
+  print (bool)
 
-q2():
-  #Write code here
+def q2():
+  let = input ("Input an integer: ")
+  bool1 = ("5" < let)
+  print(bool1)
+ 
 
-q3():
-  #Write code here
 
-q4():
-  #Write code here
+def q3():
+  sksk = input ("Input the letter a:")
+  bool2 = (sksk == "a")
+  print (bool2)
 
-q5():
-  #Write code here
+
+def q4():
+  bskibidi = input ("Input a word earlier in the dictionary than google: ")
+  bool3 = (bskibidi < "google")
+  print (bool3)
+
+def q5():
+  stephenli = int(input ("Input an integer: "))
+  ansonzhu = int(input ("Input another integer: "))
+  ohio = str(stephenli * ansonzhu)
+  bool4 = (ohio > "40")
+  skib = (f"Your numbers multiplied together are greater than 40: {bool4}")
+  print (skib)
 
 #Do edit the code below
 #Comment the lines below when running your tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+#q1()
+#q2()
+#q3()
+#q4()
+#q5()
